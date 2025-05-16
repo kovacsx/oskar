4afdc21982e5ea27648ec4ddc4afc30c8089206c
Janis Kovalevskis <janis.kovalevskis@gmail.com>
2025-05-16 13:47:16 +0300

    fix build instructions

--- containers/buildWindows.docker/start_build.bat
+++ containers/buildWindows.docker/start_build.bat
@@ -4,9 +4,9 @@ echo "ArangoDB build on windows latest started!"
 
 git clone --depth 1 --recurse-submodules --shallow-submodules --jobs 8 --branch feature/windows-clang-build https://github.com/fceller/arangodb
 
-cmake --fresh -G "Visual Studio 17 2022" -B ./build-arangodb/ -S ./build/arangodb/  --preset community -T="ClangCL" -DSKIP_PACKAGING=TRUE -DUSE_JEMALLOC=FALSE -DUSE_LIBUNWIND=FALSE -DUSE_V8=OFF -DUSE_BUILD_ID_READER=OFF
+cmake --fresh -G "Visual Studio 17 2022" -B ./build-arangodb/ -S ./arangodb/  --preset community -T="ClangCL" -DSKIP_PACKAGING=TRUE -DUSE_JEMALLOC=FALSE -DUSE_LIBUNWIND=FALSE -DUSE_V8=OFF -DUSE_BUILD_ID_READER=OFF
 
-cmake --build ./build-arangodb/
+cmake --build ./build-arangodb/ --target arangod.exe
 
 echo "To build arangod target run:"
 echo "cmake --build ./build-arangodb/ --target arangod"
